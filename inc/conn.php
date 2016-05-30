366c9a7470a66e0e92f9a61556f44e9827c04046
pengteling <pengteling@gmail.com>
2016-05-30 17:21:20 +0800

    mssql ok

--- inc/conn.php
+++ inc/conn.php
@@ -1,12 +1,30 @@
 <?php
-$con = mysql_connect("localhost","root","root");
-if (!$con)
-  {
-  die('Could not connect: ' . mysql_error());
-  }
+// $con = mysql_connect("localhost","root","root");
+// if (!$con)
+//   {
+//   die('Could not connect: ' . mysql_error());
+//   }
 
-mysql_select_db("xannad", $con);
+// mysql_select_db("xannad", $con);
 
 //echo dirname(__FILE__);
 //echo getcwd();
 
+$dsn = 'mysql:Server=127.0.0.1;database=xannad';
+$user = 'root';
+$password = 'root';
+
+$dsn = 'sqlsrv:Server=JADEBOXSERVER\SQLEXPRESS;database=xannad';
+$user = 'xannad';
+$password = 'xannadadm';
+
+try{
+	$dbh = new PDO($dsn,$user,$password);
+}catch(PDOException $e){
+	echo '数据库连接失败：'.$e->getMessage();
+}
+
+$rs = $dbh -> query("select * from category"); 
+	while($row = $rs -> fetch()){ 
+	print_r($row); 
+} 
